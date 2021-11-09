f17009f01e5d0d21ef3c1ea48cdbc15c43fc6d11
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2021-11-08 22:59:26 -0600

    Modificando reinicio de contraseña

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,104 +5,114 @@ const Op = Sequelize.Op;
 const crypto = require('crypto');
 const bcrypt = require('bcrypt-nodejs');
 
-exports.autenticarUsuario = passport.authenticate('local',{
-    successRedirect: '/',
-    failureRedirect: '/iniciar-sesion',
-    failureFlash: true,
-    badRequestMessage: 'Ambos Campos son Obligatorios'
+exports.autenticarUsuario = passport.authenticate('local', {
+	successRedirect: '/',
+	failureRedirect: '/iniciar-sesion',
+	failureFlash: true,
+	badRequestMessage: 'Ambos Campos son Obligatorios',
 });
 
 //Función para revisar si el usuario está logeado o no
-exports.usuarioAutenticado = (req, res, next) =>{
-    //Si el usuario está autenticado, adelante
-    if (req.isAuthenticated()) {
-        return next();
-    }
+exports.usuarioAutenticado = (req, res, next) => {
+	//Si el usuario está autenticado, adelante
+	if (req.isAuthenticated()) {
+		return next();
+	}
 
-    //Si no está autenticado, redirigir al formulario
-    return res.redirect('/iniciar-sesion');
-}
+	//Si no está autenticado, redirigir al formulario
+	return res.redirect('/iniciar-sesion');
+};
 
-exports.cerrarSesion = (req, res) =>{
-    req.session.destroy(() => {
-        res.redirect('/iniciar-sesion');
-    });
-}
+exports.cerrarSesion = (req, res) => {
+	req.session.destroy(() => {
+		res.redirect('/iniciar-sesion');
+	});
+};
 
 //Genera un token si el usuario es válido
-exports.enviarToken = async (req, res) =>{
-    //Verificar que el usuario exista
-    const { email } = req.body;
-    const usuario = await Usuarios.findOne({
-        where: { email }
-    });
+exports.enviarToken = async (req, res) => {
+	//Verificar que el usuario exista
+	const { email } = req.body;
 
-    //Si no existe el usuario
-    if (!usuario) {
-        req.flash('error', 'No existe esa cuenta');
-        res.redirect('/reestablecer');
-    }
-    
-    //Usuario existe
-    usuario.token = crypto.randomBytes(20).toString('hex');
-    usuario.expiracion = Date.now() + 3600000;
+	try {
+		const usuario = await Usuarios.findOne({
+			where: { email },
+		});
 
-    //Guardarlos en la BD
-    await usuario.save();
+		//Usuario existe
+		usuario.token = crypto.randomBytes(20).toString('hex');
+		usuario.expiracion = Date.now() + 3600000;
 
-    //url de reset
-    const resetUrl = `http://${req.headers.host}/reestablecer/${usuario.token}`;
-    console.log(resetUrl);
-}
+		//Guardarlos en la BD
+		await usuario.save();
 
-exports.validarToken = async (req, res) =>{
-    // res.json(req.params.token);
-    const { token } = req.params;
-    const usuario = await Usuarios.findOne({
-        where: {
-            token,
-            expiracion: {
-                [Op.gte]: Date.now()
-            }
-        }
-    });
-    
-    
-    //Si no encuentra el usuario
-    if (!usuario) {
-        req.flash('error', 'No Válido');
-        res.redirect('/reestablecer');
-    }
+		//url de reset
+		const resetUrl = `http://${req.headers.host}/reestablecer/${usuario.token}`;
+		console.log(resetUrl);
+	} catch (error) {
+		req.flash('error', 'No existe esa cuenta');
+		res.redirect('/reestablecer');
+	}
+};
 
-    //Formulario para generar el password
-    res.render('resetPassword', {
-        nombrePagina: 'Reestablecer Contraseña'
-    });
-}
+exports.validarToken = async (req, res) => {
+	// res.json(req.params.token);
+	const { token } = req.params;
+
+	//Verifica el token valido pero también la fecha de expiración
+	const usuario = await Usuarios.findOne({
+		where: {
+			token,
+			expiracion: {
+				[Op.gte]: Date.now(),
+			},
+		},
+	});
+
+	//Si no encuentra el usuario
+	if (!usuario) {
+		req.flash('error', 'No Válido');
+		res.redirect('/reestablecer');
+	}
+
+	//Formulario para generar el password
+	res.render('resetPassword', {
+		nombrePagina: 'Reestablecer Contraseña',
+	});
+};
 
 exports.actualizarPassword = async (req, res) => {
-    const {token} = req.params;
+	const { token } = req.params;
 
-    //Verifica el token valido pero también la fecha de expiración
-    const usuario = await Usuarios.findOne({
-        where: {
-            token
-        }
-    });
+	if (!req.body.password) {
+		req.flash('error', 'El password no puede ir vacío');
+		res.redirect(`/reestablecer/${token}`);
+	} else {
+		const usuario = await Usuarios.findOne({
+			where: {
+				expiracion: {
+					[Op.gte]: Date.now(),
+				},
+			},
+		});
 
-    if(!usuario){
-        req.flash('error', 'No Válido');
-        res.redirect('/reestablecer');
-    }
+		if (!usuario) {
+			req.flash('error', 'No Válido');
+			res.redirect('/reestablecer');
+		}
 
-    //Hasehar el nuevo password
-    usuario.password = bcrypt.hashSync( req.body.password, bcrypt.genSaltSync(10) );
-    usuario.token = null;
-    usuario.expiracion = null;
+		//Hasehar el nuevo password
+		usuario.password = bcrypt.hashSync(
+			req.body.password,
+			bcrypt.genSaltSync(10)
+		);
+		usuario.token = null;
+		usuario.expiracion = null;
 
-    //Guardar el nuevo password
-    await usuario.save();
+		//Guardar el nuevo password
+		await usuario.save();
 
-    req.flash('correcto', 'Tu password se ha modificado correctamente');
-    res.redirect('/iniciar-sesion');
-}
+		req.flash('correcto', 'Tu password se ha modificado correctamente');
+		res.redirect('/iniciar-sesion');
+	}
+};
